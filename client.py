e33523653e0448d10f9a06869de02fe13f97e3e2
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2020-02-23 21:37:45 +0100

    init

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+while True:
+  msg = s.recv(1024)
+  print(msg.decode("utf-8"))
+  payload = input("> ");
+  s.send(payload.encode("utf-8"))
